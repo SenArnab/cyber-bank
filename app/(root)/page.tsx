da27614a4163c006e2ba85ce51fd0f5b0851b543
SenArnab <senarnab414@gmail.com>
2025-02-24 21:28:22 +0530

    message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName : 'Arnab'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'guest'}
+            subtext="Access and manage your acount and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1769.58}
+          />
+        </header>
+
+      </div>
+    </section>
   )
 }
 
