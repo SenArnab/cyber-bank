2a84af93e8b792f7ad90d06afe161f9500fd0e2b
SenArnab <senarnab414@gmail.com>
2025-02-27 22:37:23 +0530

    implement component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
-
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,7 +11,16 @@ export default function RootLayout({
   return (
    <main className="flex h-screen w-full font-inter">
     < Sidebar user={loggedIn}/>
-    {children}
+
+    <div className="flec size-full size-col">
+      <div className="root-layout">
+        <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+        <div>
+          <MobileNav user= {loggedIn} />
+        </div>
+      </div>
+      {children}
+    </div>
    </main>
   );
 }
