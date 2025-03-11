f6f09c8c9434d806fd48c5abd227a123c4f7d3df
SenArnab <senarnab414@gmail.com>
2025-03-11 22:45:05 +0530

    Integrate appwrite and components

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,7 +20,7 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 
 
@@ -48,20 +48,20 @@ const AuthForm = ({type} : {type: string}) => {
         try {
             //  sign up appwrite
 
-            // if (type === 'sign-up') {
-            //     const newUser = await signUp(data);
-            //     setUser(newUser);
-            // }
-            // if (type === 'sign-in') {
-            //     const response = await signIn({
-            //         email: data.email,
-            //         password: data.password
-            //     })
+            if (type === 'sign-up') {
+                const newUser = await signUp(data);
+                setUser(newUser);
+            }
+            if (type === 'sign-in') {
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password
+                })
 
-            //     if (response) {
-            //         router.push('/')
-            //     }
-            // }
+                if (response) {
+                    router.push('/')
+                }
+            }
         } catch (error) {
             console.log(error)
         } finally {
